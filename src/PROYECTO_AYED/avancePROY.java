920339d919631b2a75fa6af55f35a38fef2f2bef
Noelia <Noelia@192.168.0.193>
2023-09-11 23:47:29 -0500

    Modificacion avance

--- src/PROYECTO_AYED/avancePROY.java
+++ src/PROYECTO_AYED/avancePROY.java
@@ -4,5 +4,6 @@ package PROYECTO_AYED;
 public class avancePROY {
     public static void main(String[] args) {
         System.out.println("uwu");
+        System.out.println("Gaaaaaaaaaa");
     }
 }
